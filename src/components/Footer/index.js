fbfc829d36dbc5a9599d16da50148917ec952603
Steven Baty <baty_steven@yahoo.com>
2021-05-02 02:06:00 -0600

    created utils/helpers.js.. updated Footer to pass p element, updated Nav to show Projects and Contact on Header

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Footer() {
+
+    return (
+        <div>dfsdfs</div>
+    );
+};
+
+export default Footer;
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import Projects from '../Projects';
+import ContactForm from '../Contact';
 
 function Nav() {
 
     return (
         <ul>
             <li>
-                <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+                <a data-testid="about" href="#about" >
                     About me
                 </a>
             </li>
             <li>
-                <a data-testid="projects" href="#projects" onClick={() => setContactSelected(false)}>
-                    <Projects />
+                <a data-testid="projects" href="#projects" >
+                    Projects<Projects />
                 </a>
             </li>
             <li>
-                <a data-testid="contact" href="#contact" onClick={() => setContactSelected(false)}>
-                    Contact
+                <a data-testid="contact" href="#contact" >
+                    Contact<ContactForm />
                 </a>
             </li>
         </ul>
