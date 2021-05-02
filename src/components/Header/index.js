e93b7e7fc5388932fa55971ee6c2f5b50a7c6d96
Steven Baty <baty_steven@yahoo.com>
2021-05-02 00:20:30 -0600

    updated Nav/index.js to include ABout me and Projects, updated Header/index.js to include Steven Baty and Nav..

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,2 +1,18 @@
 import React from 'react';
+import Nav from '../Nav';
 
+function Header() {
+
+    return (
+        <div>
+            <h2>
+                <a data-testid="link" href="/">
+                    Steven Baty
+                </a>
+            </h2>
+            <Nav />
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Nav() {
+
+    return (
+        <ul>
+            <li>
+                <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+                    About me
+                </a>
+            </li>
+            <li>
+                <a data-testid="projects" href="#projects" onClick={() => setContactSelected(false)}>
+                    <Projects />
+                </a>
+            </li>
+        </ul>
+
+    );
+};
+
+export default Nav;
